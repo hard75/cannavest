22439a73608f4495ced83bcff048efc4c3fd2e9a
hard75 <barrazajesusarmando@gmail.com>
2020-03-02 17:06:59 -0500

    menu principal

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="general-margin">
+    <nuxt />
+  </div>
+</template>
+
+<style>
+  html {
+    font-family: 'Open Sans', ;
+  }
+
+  .general-margin {
+    width: 1711px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      {
+        href: "https://fonts.googleapis.com/css?family=Open+Sans&display=swap",
+        rel:"stylesheet"
+      },
+      { 
+        rel: "stylesheet",
+        href: "https://use.fontawesome.com/releases/v5.8.1/css/all.css",
+        integrity: "sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf",
+        crossorigin: "anonymous" 
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^element-ui/],
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
